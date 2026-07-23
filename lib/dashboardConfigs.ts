42cc1afc6f67e5c7bad0c1642f2440c17c413166
grahito-mkr <anung.grahito@mekari.com>
2026-07-23 16:55:03 +0700

    Create dashboardConfigs.ts

---
+++ lib/dashboardConfigs.ts
@@ -0,0 +1,6 @@
+"24dd8159-b3b0-4803-b5cf-e08a65c6b897": {
+  worksheetName: "Salary Report Pagination",
+  pageSize: 5,
+  titleBase: "Report",
+  headerLines: ["DOUBLE TREE BY HILTON JAKARTA BINTARO RAYA", "REPORT SALARY CRYSTAL REPORT"]
+}
